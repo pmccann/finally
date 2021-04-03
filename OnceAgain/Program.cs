bf211ac3135b0463115c544555bcc3a6ab962f73
Paul McCann <pmccann@gmail.com>
2021-04-03 15:05:39 +1030

    Intial setup of basic console app in Visual Studio

---
+++ OnceAgain/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnceAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Other World!");
+        }
+    }
+}
